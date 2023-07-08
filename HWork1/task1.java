3f1040728bd7bb70616311bc4fb8fea5a4cd9da5
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-07-08 07:15:09 +0300

    Update task1.java
    cmpl

--- HWork1/task1.java
+++ HWork1/task1.java
@@ -6,8 +6,32 @@ package HWork1;
  * (произведение чисел от 1 до n)
  */
 public class task1 {
+    public static int calculateTriangularNumber(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static int calculateFactorial(int n) {
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число n: ");
+        int n = scanner.nextInt();
+
+        int triangularNumber = calculateTriangularNumber(n);
+        int factorial = calculateFactorial(n);
+
+        System.out.println("Треугольное число " + n + ": " + triangularNumber);
+        System.out.println("Факториал " + n + ": " + factorial);
     }
 }
